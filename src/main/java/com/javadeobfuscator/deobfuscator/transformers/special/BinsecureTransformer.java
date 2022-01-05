b1a77557a7efedf72d6e514a7406c829dbef2602
bxlachaxdd <masamun66@gmail.com>
2022-01-05 02:37:52 +0100

    Avoid using classpath

--- src/main/java/com/javadeobfuscator/deobfuscator/transformers/special/BinsecureTransformer.java
+++ src/main/java/com/javadeobfuscator/deobfuscator/transformers/special/BinsecureTransformer.java
@@ -1202,8 +1202,8 @@ public class BinsecureTransformer extends Transformer<BinsecureTransformer.Confi
                 });
             else if(ain instanceof MethodInsnNode)
             {
-                ClassNode cn = classpath.values().stream().filter(c -> c.name.equals(((MethodInsnNode)ain).owner)).findFirst().orElse(null);
-                if(cn == null)
+                ClassNode cn = classNodes().stream().filter(c -> c.name.equals(((MethodInsnNode)ain).owner)).findFirst().orElse(null);
+                if(cn == null && !((MethodInsnNode)ain).owner.startsWith("java/lang"))
                     context.customMethodFunc.put(ain, (list, ctx) -> {
                         throw new NoClassDefFoundError("Fake class");
                     });
